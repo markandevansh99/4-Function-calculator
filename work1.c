a1fc59e6249e2b06eb8dafe82d7d6b98364084a2
markandevansh99 <500069794@stu.upes.ac.in>
2019-01-29 11:54:50 +0530

    So,Here is the four function calculator

---
+++ work1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,add,sub,mul;
+	float division;
+	printf("\n Enter the value of a");
+	scanf("%d",&a);
+	printf("\n  Enter the value of b:");
+	scanf("%d",&b);
+	printf("\nAddtion is:%d ",a+b);
+	printf("\nSutraction is:%d ",a-b);
+	printf("\nMultiplication is:%d ",a*b);
+	printf("\nDivision is:%f ",(a/b));
+	return 0;
+}
+
